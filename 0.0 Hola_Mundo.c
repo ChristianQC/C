e7efe933894236bda001af666134e0abf9768e3b
ChristianQC <46302054+ChristianQC@users.noreply.github.com>
2019-09-04 19:30:24 -0500

    Create 0.0 Hola_Mundo.c

---
+++ 0.0 Hola_Mundo.c
@@ -0,0 +1,10 @@
+#include <stdio.h>  // El encabezado stdio.h define tres tipos de variables, 
+                    // varias macros y varias funciones para realizar entradas y salidas.
+
+int main()
+
+{
+
+    printf("Hola mundo");
+    getch();
+}
