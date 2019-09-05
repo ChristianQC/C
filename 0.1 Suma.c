f06fa7198dfb686b0183a18c1e1b4d675165af50
ChristianQC <46302054+ChristianQC@users.noreply.github.com>
2019-09-04 19:53:00 -0500

    Create 0.1 Suma.c

---
+++ 0.1 Suma.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+
+{
+    int num1,num2,suma;
+    printf("Ingrese Primer Valor :");
+    scanf("%i",&num1);
+    printf("Ingrese Segundo Valor :");
+    scanf("%i",&num2);
+    suma = num1+num2;
+    printf("La sumaa de los dos valores es :");
+    printf("%i",suma);
+    printf("\n");
+
+    getch();
+}
